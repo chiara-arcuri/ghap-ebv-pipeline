916dc38bc761d667fce7edb5264f4c11e5efebc1
chiara-arcuri <102859906+chiara-arcuri@users.noreply.github.com>
2025-04-10 17:35:43 +0200

    Update 03 - Run_model.R

--- 03 - Run_model.R
+++ 03 - Run_model.R
@@ -1,58 +1,80 @@
 #############################################################
-# Title: Run single-step GBLUP model
+# Title: Run single-step GBLUP model with GHap
 # Authors: Chiara Arcuri and Marco Milanesi
 # Date: 10/04/2025
 # Description:
 #   This script performs a single-step GBLUP analysis by:
-#     - Fitting a linear mixed model using GHap
+#     - Loading genotype, phenotype, and pedigree data
+#     - Loading and preparing the inverse of the H matrix (genomic + pedigree matrix)
+#     - Fitting a linear mixed model using GHap to predict breeding values (EBVs)
+#     - Saving the model environment for future use
+#
 # Requirements:
-#   - GHap package 
-#   - GHap-formatted PLINK genotype data ("output")
-#   - Phenotype file: BufRenumEBV.dat
-#   - Pedigree file: BufRenumEBVs.ped
+#   - GHap package
+#   - Matrix package
+#   - Input files:
+#     - Genotype data in PLINK format
+#     - Phenotype file (e.g., renf90GENOBU.dat)
+#     - Pedigree file (e.g., renadd01GENOBU.ped)
+#     - H-inverse matrix file (e.g., Hinv_ebv.txt)
 #############################################################
 
 # Prepare the environment
 options(stringsAsFactors = FALSE)
 options(digits = 15)
 options(scipen = 999)
-rm(list = ls())
-gc()
+rm(list = ls())  
+gc()  
 
-# Load required library
-library(GHap)
+# Load required libraries
+library(Matrix) 
+library(GHap)  
 
-# Load PLINK data in GHap format
+# Load PLINK genotype data (in GHap format)
 plink <- ghap.loadplink("output")
 
-# Load phenotype data
-df <- read.table(file = "BufRenumEBV.dat", header = FALSE, 
-                 col.names = c("id","TC","PHYS","LC","CLASSDO","SIRECODPROV",
-                               "pheno","GP","PP","G","P","RESA"))
+# Load phenotype data from a text file
+df <- read.table(file = "renf90GENOBU.dat", header = FALSE,
+                 col.names = c("id","TC","HYS","LC","DO","pheno"))
 
-# Load pedigree data (id, sire, dam)
-ped <- read.table(file = "BufRenumEBVs.ped", header = FALSE, 
-                  col.names = c("id", "sire", "dam"))
+# Load pedigree data from a text file
+ped <- read.table(file = "renadd01GENOBU.ped", header = FALSE, 
+                  col.names = c("id","sire","dam"))
 
-# Compute the genomic relationship matrix (GRM)
-K <- ghap.kinship(plink)
+# Load the inverse of the H matrix (this includes genomic and pedigree information)
+Hinv <- read.table(file = "Hinv_ebv.txt", header = FALSE)
 
-# Prepare list of all individuals for the H matrix
-ids <- unique(c(ped$id, ped$sire, ped$dam, colnames(K)))
+# Combine and sort unique levels (from V1 and V2 columns of the Hinv matrix)
+common_levels <- sort(unique(c(Hinv$V1, Hinv$V2)))
 
-# Compute the inverse of the H matrix (blended genomic and pedigree)
-Hinv <- ghap.getHinv(K = K, ped = ped, include = ids)
+# Map levels to numerical indices
+row_indices <- match(Hinv$V1, common_levels)
+col_indices <- match(Hinv$V2, common_levels)
 
-# Add replication column for random effects
-df$rep <- df$id
-
-# Fit single-step GBLUP model
-model3 <- ghap.lmm(
-  formula = pheno ~ 1 + (1|id) + (1|rep),
-  data = df,
-  covmat = list(id = Hinv, rep = NULL),
-  invcov = TRUE
+# Create a sparse matrix using the H-inverse data
+sparse_mat <- sparseMatrix(
+  i = row_indices,
+  j = col_indices,
+  x = Hinv$V3,  # Values from the matrix
+  symmetric = TRUE,
+  dimnames = list(common_levels, common_levels)  # Set row and column names
 )
 
-# Save R environment with fitted model
-save.image("model_env.RData")
+# Display the sparse matrix
+print(sparse_mat)
+
+# Run the single-step GBLUP model
+df$rep <- df$id  # Add the replication variable to the phenotype data
+
+# Fit the GBLUP model using the formula with random effects for id and rep
+model3 <- ghap.lmm(formula = pheno ~ 1 + TC + HYS + LC + DO + (1|id) + (1|rep),
+                   data = df,
+                   covmat = list(id = sparse_mat, rep = NULL),
+                   invcov = TRUE,  # Use the inverse covariance matrix
+                   errors = FALSE,  # Suppress error output
+                   vcp.initial = list(id = 84706, rep = 52892, Residual = 105940),  # Initial variance components
+                   vcp.estimate = FALSE, 
+                   em.reml = 3)  
+
+# Save the model environment
+save.image(file = 'Env_GHap.RData')
