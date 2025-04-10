9112434fdbfbc4e212e1730172d717e0792ebe2b
chiara-arcuri <102859906+chiara-arcuri@users.noreply.github.com>
2025-04-10 17:39:07 +0200

    Update 04 - EBV_analysis.R

--- 04 - EBV_analysis.R
+++ 04 - EBV_analysis.R
@@ -15,53 +15,64 @@
 #   - EBV file (xlsx format)
 #############################################################
 
-### 1. Setup environment
+# Preparation of the environment 
 options(stringsAsFactors = FALSE)
 options(digits = 15)
 options(scipen = 999)
-rm(list = ls())
-gc()
+rm(list = ls())  
+gc()  
 
-# Load required packages
+# Load necessary libraries
 library(GHap)
 library(openxlsx)
 library(ggplot2)
 library(ggExtra)
 
-### 2. Load genotype data
-plink <- ghap.loadplink("genobu_in")
+# Load PLINK genotype data
+plink <- ghap.loadplink("output")
 
-### 3. Load EBVs
+# Load EBV data
 ebv <- read.xlsx(xlsxFile = "ebv_all.xlsx", colNames = TRUE)
-ebv_gen <- ebv[which(ebv$IDA %in% plink$id), c(8,5)]
+ebv_gen <- ebv[which(ebv$IDA %in% plink$id), c(8, 5)]
+
+# Ensure that EBV data and PLINK IDs match
+identical(as.numeric(ebv_gen$IDA), as.numeric(plink$id))
 ebv_gen <- ebv_gen[order(match(ebv_gen$IDA, plink$id)), ]
+identical(as.numeric(ebv_gen$IDA), as.numeric(plink$id))
+
+# Extract reference BLUPs and create histogram
 refblup <- ebv_gen$solBLUP
 names(refblup) <- ebv_gen$IDA
+hist(refblup)
 
-### 4. Calculate kinship matrix
-K <- ghap.kinship(plink, ncores = 30)
+# Compute the genomic relationship matrix (kinship)
+K <- ghap.kinship(plink, ncores = 40)
 
-### 5. Estimate variance explained by each marker
-mkrblup <- ghap.varblup(object = plink, gebv = refblup, covmat = K, ncores = 30, verbose = TRUE)
+# Convert BLUPs of individuals into BLUPs of variants
+mkrblup <- ghap.varblup(object = plink, gebv = refblup, covmat = K, ncores = 40, verbose = TRUE)
 
-mkrblup$percVAR <- mkrblup$pVAR * 100
-write.table(mkrblup[mkrblup$percVAR > 0.5, ], "percVAR_over05.txt", sep = "\t", row.names = FALSE, quote = FALSE)
+# Summarize the pVAR values
+summary(mkrblup$pVAR)
 
-# Manhattan plots
-pdf("pVAR.pdf", 10, 7)
+# Create and save the Manhattan plot for pVAR
+pdf("pVAR.pdf", width = 10, height = 7)
 ghap.manhattan(data = mkrblup, chr = "CHR", bp = "BP", y = "pVAR")
 dev.off()
 
-pdf("percVAR.pdf", 10, 7)
-ghap.manhattan(data = mkrblup, chr = "CHR", bp = "BP", y = "percVAR",
-               main = "Explained Variance\n1 marker window",
-               xlab = "Chromosome", ylab = "% VAR")
+# Plot percentage of variance explained by SNPs
+pdf("percVAR.pdf", width = 10, height = 7)
+mkrblup$percVAR <- mkrblup$pVAR * 100
+ghap.manhattan(data = mkrblup, chr = "CHR", bp = "BP", y = "percVAR", main = "Explained Variance\nHaplotypes", xlab = "Chromosome", ylab = "% VAR")
 dev.off()
 
-### 6. Calculate GEBVs and compare to true EBVs
-gebv <- ghap.profile(object = plink, score = mkrblup, ncores = 30, verbose = TRUE)
+# Save variants explaining more than 0.5% of the variance
+write.table(x = mkrblup[which(mkrblup$percVAR > 0.5), ], file = "percVAR_over05.txt", sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
+
+# Build GEBVs from variant effects and compare predictions with reference BLUPs
+gebv <- ghap.profile(object = plink, score = mkrblup, ncores = 20, verbose = TRUE)
 hist(gebv$SCORE)
 
-# Correlation and visualization
+# Correlation between predicted GEBVs and reference BLUPs
 cor.test(gebv$SCORE, refblup)
-plot(gebv$SCORE, refblup); abline(0, 1)
+plot(gebv$SCORE, refblup, main = "Correlation by Haplotypes", xlab = "EBVs computed by GHap", ylab = "EBVs computed by ANASB")
+abline(0, 1)
